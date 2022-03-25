b503db8c00a2ea6dd955b78ab325a22282874014
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-03-25 18:35:01 +0200

    adding exercises and learning to use git in the process :)

---
+++ Basic-Synthax,Conditional-Statements and Loops/rounding.js
@@ -0,0 +1,9 @@
+function rounding(num, precision) {
+  num = Number(num);
+  precision = Number(precision);
+  if (precision > 15) {
+    precision = 15;
+  }
+  num = num.toFixed(precision);
+  console.log(parseFloat(num));
+}
