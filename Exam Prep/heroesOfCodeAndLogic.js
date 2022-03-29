75fa6761696a397bd2d2d8f9b754dd615bde667c
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-03-29 20:49:43 +0300

    100/100 heroesOfCode

--- Exam Prep/heroesOfCodeAndLogic.js
+++ Exam Prep/heroesOfCodeAndLogic.js
@@ -1,68 +1,87 @@
 function solve(input) {
+    // take number of heroes
     let n = Number(input.shift());
-    let obj = {};
+    let heroes = {};
+
     for(let i = 0; i < n; i++) {
-        let tokens = input.shift().split(" ");
-        let [hero,hp,mp] = tokens;
-        hp = Number(hp);
-        mp = Number(mp);
-        if(hp <= 100 && mp <= 200) {
-            obj[hero] = {
-                hp,
-                mp
-            }
+        // - create new hero
+        let heroData = input.shift().split(" ");
+        let name = heroData[0];
+        let hp = Number(heroData[1]);
+        let mp = Number(heroData[2]);
+        heroes[name] = {
+            hp,
+            mp
         }
     }
-    for(let el of input) {
-        let [command,hero,par,name] = el.split(" - ");
-        par = +par;
-        if(command == "End") {
+
+    while(input[0] != "End") {
+        let tokens = input.shift().split(" - ");
+        let command = tokens[0];
+        let name = tokens[1];
+        let p1 = Number(tokens[2]);
+        let p2 = tokens[3];
+
+        let hero = heroes[name];
+
+        if (command == "End") {
             break;
         }
-        if(command == "CastSpell") {
-            if(obj[hero].mp > par) {
-                obj[hero].mp -= par;
-                console.log(`${hero} has successfully cast ${name} and now has ${obj[hero].mp} MP!`);
+        else if(command == "CastSpell") {
+            let mana = Number(p1);
+            if (hero.mp >= p1) {
+                hero.mp -= p1;
+                console.log(`${name} has successfully cast ${p2} and now has ${hero.mp} MP!`);
+            } else {
+                console.log(`${name} does not have enough MP to cast ${p2}!`);
             }
-        }
-        if(command == "TakeDamage") {
-            if(obj[hero].hp > par) {
-                obj[hero].hp -= par;
-                if(obj[hero].hp > 0) {
-                    console.log(`${hero} was hit for ${par} HP by ${name} and now has ${obj[hero].hp} HP left!`);
-                } else {
-                    console.log(`${hero} has been killed by ${name}!`);
-                    delete obj[hero];
-                }
+
+        } else if (command == "TakeDamage") {
+            hero.hp -= p1;
+            if (hero.hp > 0) {
+                console.log(`${name} was hit for ${p1} HP by ${p2} and now has ${hero.hp} HP left!`);
+            } else {
+                delete heroes[name];
+                console.log(`${name} has been killed by ${p2}!`);
             }
-        }
-        if(command == "Recharge") {
-            let temp = obj[hero].mp;
-            let check = temp += par;
-            if(check <= 200) {
-                obj[hero].mp += par;
-                console.log(`${hero} recharged for ${par} MP!`);
-            }
-        }
-        if(command == "Heal") {
-            let temp = obj[hero].hp;
-            let check = temp += par;
-            if(check <= 100) {
-                obj[hero].hp += par;
-                console.log(`${hero} healed for ${par} HP!`);
-            }   
-        }
+        } else if (command == "Recharge") {
+            let mp = Math.min((200 - hero.mp), p1);
+            hero.mp += mp;
+            console.log(`${name} recharged for ${mp} MP!`);
+        } else if (command == "Heal") {
+            let hp = Math.min((100 - hero.hp), p1);
+            hero.hp += hp;
+            console.log(`${name} healed for ${hp} HP!`);
+        } 
     }
-    for(let el in obj) {
-        console.log(`${el}\nHP: ${obj[el].hp}\nMP: ${obj[el].mp}`);
+
+    for (let heroData of Object.entries(heroes)) {
+        let name = heroData[0];
+        let hero = heroData[1];
+        console.log(name);
+        console.log(`  HP: ${hero.hp}`);
+        console.log(`  MP: ${hero.mp}`);
     }
 }
-solve([2,
-    "Solmyr 85 120",
-    "Kyrre 99 50",
-    "Heal - Solmyr - 10",
-    "Recharge - Solmyr - 50",
-    "TakeDamage - Kyrre - 66 - Orc",
-    "CastSpell - Kyrre - 15 - ViewEarth",
-    "End"]
+// solve([2,
+//     "Solmyr 85 120",
+//     "Kyrre 99 50",
+//     "Heal - Solmyr - 10",
+//     "Recharge - Solmyr - 50",
+//     "TakeDamage - Kyrre - 66 - Orc",
+//     "CastSpell - Kyrre - 15 - ViewEarth",
+//     "End"
+// ])
+solve(["4",
+    "Adela 90 150",
+    "SirMullich 70 40",
+    "Ivor 1 111",
+    "Tyris 94 61",
+    "Heal - SirMullich - 50",
+    "Recharge - Adela - 100",
+    "CastSpell - Tyris - 1000 - Fireball",
+    "TakeDamage - Tyris - 99 - Fireball",
+    "TakeDamage - Ivor - 3 - Mosquito",
+    "End"
+    ]
     )
