ca35f182ca243b7d16282ad41864f50f7dbb631b
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-03-30 20:05:34 +0300

    Commented out input

--- Exam Prep/hocl7.js
+++ Exam Prep/hocl7.js
@@ -72,16 +72,16 @@ function solve(input) {
 //     "CastSpell - Kyrre - 15 - ViewEarth",
 //     "End"
 // ])
-solve(["4",
-    "Adela 90 150",
-    "SirMullich 70 40",
-    "Ivor 1 111",
-    "Tyris 94 61",
-    "Heal - SirMullich - 50",
-    "Recharge - Adela - 100",
-    "CastSpell - Tyris - 1000 - Fireball",
-    "TakeDamage - Tyris - 99 - Fireball",
-    "TakeDamage - Ivor - 3 - Mosquito",
-    "End"
-    ]
-    )
+// solve(["4",
+//     "Adela 90 150",
+//     "SirMullich 70 40",
+//     "Ivor 1 111",
+//     "Tyris 94 61",
+//     "Heal - SirMullich - 50",
+//     "Recharge - Adela - 100",
+//     "CastSpell - Tyris - 1000 - Fireball",
+//     "TakeDamage - Tyris - 99 - Fireball",
+//     "TakeDamage - Ivor - 3 - Mosquito",
+//     "End"
+//     ]
+//     )
