ce6d068314b41b8536a124dd011b11dc1d7671b8
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-03-31 16:11:13 +0300

    Remove 1 ex and modiifed a bunch of others

--- Exam Prep/secretChat.js
+++ Exam Prep/secretChat.js
@@ -1,41 +1,38 @@
-function solve (input) {
-    function reversedStr(str) {
-        arr = str.split("");
-        arr.reverse();
-        return (arr.join(""))
-    }
-    let message = input.shift();
-    for(let el of input) {
-        let tokens = el.split(":|:");
-        let [command,arg,replacement] = tokens;
-        if(command == "Reveal") {
-            break;
+function solve(input) {
+  function reversedStr(str) {
+    arr = str.split("");
+    arr.reverse();
+    return arr.join("");
+  }
+  let message = input.shift();
+  for (let el of input) {
+    let tokens = el.split(":|:");
+    let [command, arg, replacement] = tokens;
+    if (command == "Reveal") {
+      break;
+    } else if (command == "InsertSpace") {
+      let singlespace = " ";
+      message = message.split("");
+      message.splice(arg, 0, singlespace);
+      message = message.join("");
+      console.log(message);
+    } else if (command == "ChangeAll") {
+      message = message.replaceAll(arg, replacement);
+      console.log(message);
+    } else if (command == "Reverse") {
+      if (message.includes(arg)) {
+        let reversed = reversedStr(arg);
+        message = message.replace(arg, "");
+        message = message.split("");
+        for (let i = 0; i < reversed.length; i++) {
+          message.push(reversed[i]);
         }
-        else if(command == "InsertSpace") {
-            let singlespace = " ";
-            message = message.split("");
-            message.splice(arg,0,singlespace);
-            message = message.join("");
-            console.log(message);
-
-        } else if (command == "ChangeAll") {
-            message = message.replaceAll(arg,replacement);
-            console.log(message);
-        } 
-        else if (command == "Reverse") {
-            if(message.includes(arg)) {
-                let reversed = reversedStr(arg);
-                message = message.replace(arg, "");
-                message = message.split("");
-                for(let i = 0; i < reversed.length; i++) {
-                    message.push(reversed[i]);
-                }
-                message = message.join("");
-                console.log(message);
-            } else {
-                console.log("error");
-            }
-        }
+        message = message.join("");
+        console.log(message);
+      } else {
+        console.log("error");
+      }
     }
-    console.log(`You have a new text message: ${message}`);
+  }
+  console.log(`You have a new text message: ${message}`);
 }
