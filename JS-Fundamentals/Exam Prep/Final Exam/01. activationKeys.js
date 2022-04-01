c37ecfb93f82af0ad9704135a83b2b976ee1be39
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-04-01 18:45:27 +0300

    Added input for activKeys and passReset

---
+++ JS-Fundamentals/Exam Prep/Final Exam/01. activationKeys.js
@@ -0,0 +1,21 @@
+function solve(input) {}
+
+solve([
+  "134softsf5ftuni2020rockz42",
+  "Slice>>>3>>>7",
+  "Contains>>>-rock",
+  "Contains>>>-uni-",
+  "Contains>>>-rocks",
+  "Flip>>>Upper>>>2>>>8",
+  "Flip>>>Lower>>>5>>>11",
+  "Generate",
+]);
+// solve([
+//   "abcdefghijklmnopqrstuvwxyz",
+//   "Slice>>>2>>>6",
+//   "Flip>>>Upper>>>3>>>14",
+//   "Flip>>>Lower>>>5>>>7",
+//   "Contains>>>def",
+//   "Contains>>>deF",
+//   "Generate",
+// ]);
---
+++ JS-Fundamentals/Exam Prep/Final Exam/01. passwordReset.js
@@ -0,0 +1,17 @@
+function solve(input) {}
+solve([
+  "Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr",
+  "TakeOdd",
+  "Cut 15 3",
+  "Substitute :: -",
+  "Substitute | ^",
+  "Done",
+]);
+// solve([
+//   "up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy",
+//   "TakeOdd",
+//   "Cut 18 2",
+//   "Substitute ! ***",
+//   "Substitute ? .!.",
+//   "Done",
+// ]);
