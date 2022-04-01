955de12001cc41e194e9971246730cb33aadf00e
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-04-02 00:38:51 +0300

    Added activation keys code

--- JS-Fundamentals/Exam Prep/Final Exam/01. activationKeys.js
+++ JS-Fundamentals/Exam Prep/Final Exam/01. activationKeys.js
@@ -1,4 +1,40 @@
-function solve(input) {}
+function solve(input) {
+  let rawKey = input.shift();
+  for (let line of input) {
+    let currLine = line.split(">>>");
+    let command = currLine[0];
+    let substringOrDirection = currLine[1];
+    if (command == "Generate") {
+      break;
+    }
+    if (command == "Contains") {
+      if (rawKey.includes(substringOrDirection)) {
+        console.log(`${rawKey} contains ${substringOrDirection}`);
+      } else {
+        console.log("Substring not found!");
+      }
+    }
+    if (command == "Flip") {
+      let str = rawKey.substring(currLine[2], currLine[3]);
+      if (substringOrDirection == "Upper") {
+        let temp = str.toUpperCase();
+        rawKey = rawKey.replace(str, temp);
+        console.log(rawKey);
+      }
+      if (substringOrDirection == "Lower") {
+        let temp = str.toLowerCase();
+        rawKey = rawKey.replace(str, temp);
+        console.log(rawKey);
+      }
+    }
+    if (command == "Slice") {
+      let str = rawKey.substring(substringOrDirection, currLine[2]);
+      rawKey = rawKey.replace(str, "");
+      console.log(rawKey);
+    }
+  }
+  console.log(`Your activation key is: ${rawKey}`);
+}
 
 solve([
   "134softsf5ftuni2020rockz42",
