41bc69b82f060844f979bd7b62017faf58741386
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-04-01 17:14:15 +0300

    Added world tour

--- JS-Fundamentals/Exam Prep/Final Exam/04. worldTour.js
+++ JS-Fundamentals/Exam Prep/Final Exam/04. worldTour.js
@@ -1,4 +1,38 @@
-function solve(input) {}
+function solve(input) {
+  let stops = input.shift();
+  for (let line of input) {
+    let currLine = line.split(":");
+    if (currLine[0] == "Add Stop") {
+      stops = stops.split("");
+      if (stops[currLine[1]] != undefined) {
+        stops.splice(+currLine[1], 0, currLine[2]);
+      }
+      stops = stops.join("");
+      console.log(stops);
+    }
+    if (currLine[0] == "Remove Stop") {
+      let startIndex = +currLine[1];
+      let endIndex = +currLine[2];
+      if (stops[startIndex] && stops[endIndex]) {
+        let start = stops.substring(0, startIndex);
+        let end = stops.substring(endIndex + 1);
+        stops = start + end;
+      }
+      console.log(stops);
+    }
+    if (currLine[0] == "Switch") {
+      let oldString = currLine[1];
+      let newString = currLine[2];
+      if (stops.includes(oldString)) {
+        stops = stops.replace(oldString, newString);
+      }
+      console.log(stops);
+    }
+    if (currLine[0] == "Travel") {
+      console.log(`Ready for world tour! Planned stops: ${stops}`);
+    }
+  }
+}
 solve([
   "Hawai::Cyprys-Greece",
   "Add Stop:7:Rome",
