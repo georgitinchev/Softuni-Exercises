64201d05693d92bf31ccf7ad68ac052c53936c96
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-04-02 02:20:23 +0300

    added code

--- JS-Fundamentals/Exam Prep/Final Exam/01. passwordReset.js
+++ JS-Fundamentals/Exam Prep/Final Exam/01. passwordReset.js
@@ -1,4 +1,40 @@
-function solve(input) {}
+function solve(input) {
+  let raw = input.shift();
+  let pass = "";
+  for (let line of input) {
+    let currLine = line.split(" ");
+    let command = currLine[0];
+    if (command == "Done") {
+      console.log(`Your password is: ${pass}`);
+    }
+    if (command == "TakeOdd") {
+      pass = [];
+      for (let i = 0; i < raw.length; i++) {
+        if (i % 2 != 0) {
+          pass.push(raw[i]);
+        }
+      }
+      pass = pass.join("");
+      console.log(pass);
+    }
+    if (command == "Cut") {
+      let indexStart = +currLine[1];
+      let indexEnd = Number(currLine[1]) + Number(currLine[2]);
+      let str = pass.substring(indexStart, indexEnd);
+      pass = pass.replace(str, "");
+      console.log(pass);
+    }
+    if (command == "Substitute") {
+      if (pass.includes(currLine[1])) {
+        let regex = new RegExp(currLine[1], "g");
+        pass = pass.replace(regex, currLine[2]);
+        console.log(pass);
+      } else {
+        console.log("Nothing to replace!");
+      }
+    }
+  }
+}
 solve([
   "Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr",
   "TakeOdd",
