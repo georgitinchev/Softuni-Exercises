7fbc5467122419eb78978d85469061eaf4f6cb2b
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-05-30 10:18:49 +0300

    Added magic matrices

--- JS-Advanced/Arrays and Nested Arrays/sortArrBy2Crit.js
+++ JS-Advanced/Arrays and Nested Arrays/sortArrBy2Crit.js
@@ -1,6 +1,7 @@
 function solve(arr) {
   arr.sort(twoCritSort);
   return arr.join("\n");
+
   function twoCritSort(current, next) {
     if (current.length === next.length) {
       return current.localeCompare(next);
@@ -9,5 +10,5 @@ function solve(arr) {
   }
 }
 solve(["alpha", "beta", "gamma"]);
-console.log(solve(["Isaac", "Theodor", "Jack", "Harrison", "George"]));
+solve(["Isaac", "Theodor", "Jack", "Harrison", "George"]);
 solve(["test", "Deny", "omen", "Default"]);
