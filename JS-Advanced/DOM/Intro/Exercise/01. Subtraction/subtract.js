3fd0008edf4973d140d69e49d7cf9496ed17647b
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-06-06 21:22:01 +0300

    Added exercise

---
+++ JS-Advanced/DOM/Intro/Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+  let first = Number(document.getElementById("firstNumber").value);
+  let second = Number(document.getElementById("secondNumber").value);
+  let result = document.getElementById("result");
+  let sum = first - second;
+  result.textContent = sum;
+}
