423020cf4d9825a49e64c6b8a072b120c2a8237a
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-06-06 22:05:54 +0300

    Added pascal exercise

--- JS-Advanced/DOM/Intro/Exercise/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/DOM/Intro/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  let first = document.getElementById("text").value;
+  let second = document.getElementById("naming-convention").value;
+  let result = document.getElementById("result");
+  if (second == "Camel Case") {
+    first = first.toLowerCase();
+    first = first.split(" ");
+    for (let i = 1; i < first.length; i++) {
+      let letter = first[i].charAt(0).toUpperCase();
+      first[i] = first[i].split("");
+      first[i][0] = letter;
+      first[i] = first[i].join("");
+    }
+    first = first.join("");
+    result.innerText = first;
+  } else if (second == "Pascal Case") {
+    first = first.toLowerCase();
+    first = first.split(" ");
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i].charAt(0).toUpperCase();
+      first[i] = first[i].split("");
+      first[i][0] = letter;
+      first[i] = first[i].join("");
+    }
+    first = first.join("");
+    result.innerText = first;
+  } else {
+    result.innerText = "Error!";
+  }
 }
