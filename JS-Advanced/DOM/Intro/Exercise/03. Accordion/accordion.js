466041d068aafffa1d857527e05ae2b7d97bb7ac
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-06-06 22:24:05 +0300

    added accordion

--- JS-Advanced/DOM/Intro/Exercise/03. Accordion/accordion.js
+++ JS-Advanced/DOM/Intro/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+  let text = document.getElementsByClassName("button")[0];
+  if (text.textContent == "Less") {
+    text.textContent = "More";
+  } else {
+    text.textContent = "Less";
+  }
+  let extra = document.getElementById("extra");
+  if (extra.style.display == "none") {
+    extra.style.display = "block";
+  } else {
+    extra.style.display = "none";
+  }
 }
