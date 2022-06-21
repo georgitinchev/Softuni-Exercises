5932d7e35cc1f59fe2cef81e2b28d26f3d7000cc
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-06-21 22:36:40 +0300

    added 03.

---
+++ JS-Advanced/Advanced Functions/Exercise/02. argumentInfo.js
@@ -0,0 +1,18 @@
+function solve(...args) {
+  const params = Array.from(arguments);
+  const types = {};
+  for (let arg of params) {
+    const type = typeof arg;
+    console.log(`${type}: ${arg}`);
+
+    if (types[type] == undefined) {
+      types[type] = 0;
+    }
+    types[type]++;
+  }
+  const result = Object.entries(types).sort((a, b) => b[1] - a[1]);
+
+  for (let [type, count] of result) {
+    console.log(`${type} = ${count}`);
+  }
+}
---
+++ JS-Advanced/Advanced Functions/Exercise/03. fibonacci.js
@@ -0,0 +1,22 @@
+function getFibonator() {
+  let prev = 0;
+  let curr = 1;
+
+  return function () {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+
+    return prev;
+  };
+}
+
+let fib = getFibonator();
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
