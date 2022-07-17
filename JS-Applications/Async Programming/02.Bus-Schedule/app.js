13ad2b86e054f934119b675c51953bbb8bb7d74f
Georgi Tinchev <georgi.tinchev.124@gmail.com>
2022-07-17 11:51:25 +0300

    solved bus schedule

--- JS-Applications/Async Programming/02.Bus-Schedule/app.js
+++ JS-Applications/Async Programming/02.Bus-Schedule/app.js
@@ -1,17 +1,38 @@
 function solve() {
+  const html = {
+    arrive: document.getElementById("arrive"),
+    depart: document.getElementById("depart"),
+    info: document.getElementsByClassName("info")[0],
+  };
+  let ID = "depot";
+  let stopName;
+  async function depart() {
+    await fetch(`http://localhost:3030/jsonstore/bus/schedule/${ID}`)
+      .then((response) => response.json())
+      .then((data) => {
+        stopName = data.name;
+        ID = data.next;
+        html.info.textContent = `Next stop ${stopName}`;
+        html.depart.disabled = true;
+        html.arrive.disabled = false;
+      })
+      .catch((error) => {
+        html.info.textContent = "Error";
+        html.arrive.disabled = true;
+        html.depart.disabled = true;
+      });
+  }
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  function arrive() {
+    html.info.textContent = `Arriving at ${stopName}`;
+    html.arrive.disabled = true;
+    html.depart.disabled = false;
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
-
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
