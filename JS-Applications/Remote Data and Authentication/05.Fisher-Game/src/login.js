4cc6bf8ffc0356f4822b4d73f3f52176ab27b4e8
Georgi Tinchev <georgi.tinchev.124@gmail.com>
2022-07-26 09:09:03 +0000

    added spa

--- JS-Applications/Remote Data and Authentication/05.Fisher-Game/src/login.js
+++ JS-Applications/Remote Data and Authentication/05.Fisher-Game/src/login.js
@@ -1,1 +1,13 @@
-console.log('TODO:// Implement Login functionality');
+const html = {
+  userNameField: document.getElementsByName("email")[0],
+  passwordInputField: document.getElementsByName("password")[0],
+  loginBtn:
+    document.getElementsByClassName("notification")[0].nextElementSibling,
+};
+
+html.loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetch(`http://localhost:3030/users/login`)
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+});
